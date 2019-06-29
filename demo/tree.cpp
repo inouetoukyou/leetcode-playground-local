f6e80e59809284821daa37b68991349a1aeef4ec
杨 明航(Minghang Yang) <inouetoukyou@outlook.com>
2019-06-29 23:00:47 +0800

    update tree.cpp

--- demo/tree.cpp
+++ demo/tree.cpp
@@ -12,12 +12,15 @@ using TreeNode = GenericTreeNode<T>;
 
 int main() {
     string tree_string;
-    getline(cin, tree_string);
+//    getline(cin, tree_string);
+    tree_string = "[1, null, 2  ,  4,7, 9]";
     TreeNode *root = TreeNode::stringToTreeNode(tree_string);
     vector<T> ans = TreeNode::preorderTraverse(root);
     for (int & an : ans) {
         cout << an << " ";
     }
     cout << endl;
+    /* output
+     * 1 2 4 9 7*/
     return 0;
 }
