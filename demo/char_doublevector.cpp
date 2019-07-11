49240dde048cf2ad6c3a6b1c5cf4d0ba7f3bce37
杨 明航(Minghang Yang) <inouetoukyou@outlook.com>
2019-07-12 02:18:10 +0800

    advancement 1) add vector<char> 2) make CMakeLists more elegant

---
+++ demo/char_doublevector.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Minghang Yang on 2019-07-12.
+//
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include "../leetcode/vectortool"
+
+using namespace std;
+using namespace ymh;
+using T = char;
+using VectorGenerator = GenericVectorGenerator<T>;
+
+int main() {
+    string input = "[[\"1\",\"e\",   \"1\"],[\"q\",  \"0\",  \"x\"]]";
+    vector<vector<char>> ans = VectorGenerator::getDoubleVector(input);
+    for (vector<char> &an : ans) {
+        for (char &a : an) {
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+    /*
+     * output
+     * 1 e 1
+       q 0 x */
+    return 0;
+}
