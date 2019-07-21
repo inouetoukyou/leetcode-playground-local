9eb2fdfe51ec970ff689966748806008fe29306f
杨 明航(Minghang Yang) <inouetoukyou@outlook.com>
2019-07-22 03:44:29 +0800

    add test.cpp to quick start 1) test.cpp is a simple tutorial for users 2) it shows how to construct ListNode, TreeNode, vector and handle input    by using a file

---
+++ test.cpp
@@ -0,0 +1,44 @@
+#include "helper.h"
+using namespace std;
+
+using T = int;
+using ListNode = ymh::GenericListNode<T>;
+using TreeNode = ymh::GenericTreeNode<T>;
+string fileSource = "../../input.txt";
+
+class Solution {
+
+};
+
+int main() {
+    // test ListNode
+    cout << "traverse ListNode:\n\t";
+    string inputList = "[3,2,0,-4]";
+    ListNode *head = getList(inputList);
+    vector<int> vList = traverseList(head);
+    outputVector(vList);
+
+    // test TreeNode
+    cout << "traverse TreeNode:\n\t";
+    string inputTree = "[1, null, 2  ,  4,7, 9]";
+    TreeNode *root = getTree(inputTree);
+    vector<int> vTree = inorderTraverseTree(root);
+    outputVector(vTree);
+
+    // test vector
+    cout << "output vector:\n\t";
+    string inputVector = "[9,1,2,3,9]";
+    vector<int> v = getVector(inputVector);
+    outputVector(v);
+
+    // using file to handle big input
+    ifstream in = getIfstream(fileSource);
+    if (in) {
+        string firstLine;
+        getline(in, firstLine);
+        cout << "firstLine of file:\n\t";
+        cout << firstLine << endl;
+        in.close();
+    }
+    return 0;
+}
