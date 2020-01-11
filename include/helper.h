a8f55f51d4aac3eaf415c722d95ba171adba6158
杨 明航(Minghang Yang) <inouetoukyou@outlook.com>
2020-01-11 14:12:04 +0800

    add function sugar outputList 1) previously, we use outputVector(traverseList(list_head))    now, we can outputList(list_head)

--- include/helper.h
+++ include/helper.h
@@ -18,53 +18,15 @@
 std::ifstream getIfstream(std:: string source) {
     return std::ifstream(source);
 }
+
 template <class T = int>
 std::vector<T> getVector(std::string &input, char delim = ',') {
     return ymh::GenericVectorGenerator<T>::getSingleVector(input, delim);
 }
+
 template <class T = int>
 std::vector<std::vector<T>> getVector2(std::string &input, char delim = ',') {
     return ymh::GenericVectorGenerator<T>::getDoubleVector(input, delim);
-}
-
-template <class T = int>
-ymh::GenericListNode<T>* getList(std::string &input, ymh::OJ_TYPE type = ymh::GenericListNode<T>::LEETCODE, char delim = ',') {
-    return ymh::GenericListNode<T>::stringToListNode(input, type, delim);
-}
-
-template <class T = int>
-ymh::GenericListNode<T>* getListLintCode(std::string &input) {
-    return getList(input, ymh::GenericListNode<T>::LINTCODE, '>');
-}
-
-template <class T>
-ymh::GenericListNode<T>* setCircle(ymh::GenericListNode<T> *head) {
-    return ymh::GenericListNode<T>::setCircle(head);
-}
-
-template <class T>
-std::vector<T> traverseList(ymh::GenericListNode<T> *head) {
-    return ymh::GenericListNode<T>::traverse(head);
-}
-
-template <class T = int>
-ymh::GenericTreeNode<T> *getTree(std::string &input, char delim = ',') {
-    return ymh::GenericTreeNode<T>::stringToTreeNode(input, delim);
-}
-
-template <class T>
-std::vector<int> preorderTraverseTree(ymh::GenericTreeNode<T> *root) {
-    return ymh::GenericTreeNode<T>::preorderTraverse(root);
-}
-
-template <class T>
-std::vector<int> postorderTraverseTree(ymh::GenericTreeNode<T> *root) {
-    return ymh::GenericTreeNode<T>::postorderTraverse(root);
-}
-
-template <class T>
-std::vector<int> inorderTraverseTree(ymh::GenericTreeNode<T> *root) {
-    return ymh::GenericTreeNode<T>::inorderTraverse(root);
 }
 
 template <class T>
@@ -90,6 +52,52 @@ void outputVector2(std::vector<std::vector<T>> &ans, char piv = ' ') {
 template <class T>
 void outputVector2(std::vector<std::vector<T>> &&ans, char piv = ' ') {
     outputVector2(ans, piv);
+}
+template <class T = int>
+ymh::GenericListNode<T>* getList(std::string &input, ymh::OJ_TYPE type = ymh::GenericListNode<T>::LEETCODE, char delim = ',') {
+    return ymh::GenericListNode<T>::stringToListNode(input, type, delim);
+}
+
+template <class T = int>
+ymh::GenericListNode<T>* getListLintCode(std::string &input) {
+    return getList(input, ymh::GenericListNode<T>::LINTCODE, '>');
+}
+
+#ifdef OJ_LISTNODE_CIRCLE
+template <class T>
+ymh::GenericListNode<T>* setCircle(ymh::GenericListNode<T> *head) {
+    return ymh::GenericListNode<T>::setCircle(head);
+}
+#endif
+
+template <class T>
+std::vector<T> traverseList(ymh::GenericListNode<T> *head) {
+    return ymh::GenericListNode<T>::traverse(head);
+}
+
+template <class T>
+void outputList(ymh::GenericListNode<T> *head) {
+    outputVector(traverseList(head));
+}
+
+template <class T = int>
+ymh::GenericTreeNode<T> *getTree(std::string &input, char delim = ',') {
+    return ymh::GenericTreeNode<T>::stringToTreeNode(input, delim);
+}
+
+template <class T>
+std::vector<int> preorderTraverseTree(ymh::GenericTreeNode<T> *root) {
+    return ymh::GenericTreeNode<T>::preorderTraverse(root);
+}
+
+template <class T>
+std::vector<int> postorderTraverseTree(ymh::GenericTreeNode<T> *root) {
+    return ymh::GenericTreeNode<T>::postorderTraverse(root);
+}
+
+template <class T>
+std::vector<int> inorderTraverseTree(ymh::GenericTreeNode<T> *root) {
+    return ymh::GenericTreeNode<T>::inorderTraverse(root);
 }
 
 int subRoutine(int argc, char *argv[], int(*sub)(), const char* relative = "/../../../input/solutions/") {
