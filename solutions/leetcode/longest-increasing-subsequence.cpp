f12883caa04c74a4d4c03f6e00eff6ea5926a9c2
ytlw <ytlw@outlook.com>
2021-02-05 00:09:19 +0800

    [Refactor][New Solutions] Part II 300 Longest Increasing Subsequence (nlog(n))

--- solutions/leetcode/longest-increasing-subsequence.cpp
+++ solutions/leetcode/longest-increasing-subsequence.cpp
@@ -31,21 +31,25 @@ public:
         return ans;
     }
     // O(nlog(n))
+    // use a vector dp with cap n to store sequence
+    // the smaller the dp, the longer the LIS may be
+    // finally, the len of dp is the ans
+    // note: the dp may not be the right LIS, but the
+    // len is wanted
     int lengthOfLIS2(vector<int>& nums) {
         int n = nums.size();
         if (n < 2) {
             return n;
         }
-        vector<int> dp(n, 1);
-        int ans = dp[0];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        int ans = 1;
         for (int i = 1; i < n; ++i) {
-            for (int j = i - 1; j >= 0; --j) {
-                if (nums[i] > nums[j] && dp[i] <= dp[j]) {
-                    dp[i] = dp[j] + 1;
-                }
-            }
-            if (dp[i] > ans) {
-                ans = dp[i];
+            if (nums[i] > dp[ans - 1]) {
+                dp[ans++] = nums[i];
+            } else {
+                auto pos = lower_bound(dp.begin(), dp.begin() + ans, nums[i]) - dp.begin();
+                dp[pos] = nums[i];
             }
         }
         return ans;
