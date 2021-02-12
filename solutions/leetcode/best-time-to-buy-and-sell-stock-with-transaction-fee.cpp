afdd5b54ca0fbc874d2ce882b2babf6e917b480d
ytlw <ytlw@outlook.com>
2021-02-13 01:10:54 +0800

    [Refactor][New Solutions] Part IV 714 Best Time to Buy and Sell Stock with Transaction Fee

--- solutions/leetcode/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ solutions/leetcode/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,12 +2,15 @@
 
 // 714. Best Time to Buy and Sell Stock with Transaction Fee
 
-// Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i;
+// Your are given an array of integers prices,
+// for which the i-th element is the price of a given stock on day i;
 // and a non-negative integer fee representing a transaction fee.
 //
-// You may complete as many transactions as you like, but you need to pay the transaction fee for each sell transaction.
+// You may complete as many transactions as you like,
+// but you need to pay the transaction fee for each sell transaction.
 //
-// You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+// You may not buy more than 1 share of a stock at a time
+// (ie. you must sell the stock share before you buy again.)
 //
 // Return the maximum profit you can make.
 #include "leetcode.h"
@@ -21,18 +24,33 @@ public:
         if (n < 2) {
             return 0;
         }
-        int ans = 0;
-        int i = 1;
-        while (i < n) {
-            while (i < n && prices[i] <= prices[i - 1]) {
-                ++i;
-            }
-            int valley = prices[i - 1];
-            ++i;
-            while (i < n && prices[i])
-            ++i;
+        int sell = 0, own = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            tie(sell, own) = make_tuple(
+                max(sell, own + prices[i] - fee),
+                max(own, sell - prices[i]));
         }
-        return ans;
+        return sell;
+    }
+
+    int maxProfitGreedy(vector<int>& prices, int fee) {
+        int n = prices.size();
+        if (n < 2) {
+            return 0;
+        }
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                // update `buy`
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                // we can regret if there is a higher price
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
     }
 };
 
@@ -47,7 +65,8 @@ int sub() {
         int fee;
         cin >> fee;
         cin.ignore(1);
-        cout << solution.maxProfit(prices, fee) << endl;
+        cout << solution.maxProfit(prices, fee) << ' ' <<
+        solution.maxProfitGreedy(prices, fee) << endl;
     }
     return 0;
 }
