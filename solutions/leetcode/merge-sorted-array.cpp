468ff4f2d3e19f996380f3bc592b0e1eb76c188c
杨明航 <yangminghang@bytedance.com>
2021-03-04 13:21:12 +0800

    [LeetCode] 88 Merge Sorted Array try 2

--- solutions/leetcode/merge-sorted-array.cpp
+++ solutions/leetcode/merge-sorted-array.cpp
@@ -18,7 +18,7 @@ public:
         int k = m + n - 1;
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums2[i--];
+                nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
             }
